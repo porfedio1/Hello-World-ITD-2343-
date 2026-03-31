7a807805ff2ecd16352ba67f75e47b9cbe29668a
porfedio1 <rafael.gonzales@okstate.edu>
2026-03-31 10:34:18 -0500

    Add files via upload

---
+++ Section 1 ExamLab_Gonzles_Solution/Exam 1 Lab - Teletype Simulation_Gonzles_Project/Form1.cs
@@ -0,0 +1,232 @@
+namespace Exam_1_Lab___Teletype_Simulation_Gonzles_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnB_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "B";//btnB puts B in the textbox.
+        }
+
+        private void btnBlank_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + " ";//btnBlank puts blankspace in the textbox.
+        }
+
+        private void btnK_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "K";//btnK puts K in the textbox.
+        }
+
+        private void btnM_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "M";//btnM puts M in the textbox.
+        }
+
+        private void btnN_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "N";//btnN puts N in the textbox.
+        }
+
+        private void btnO_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "O";//btnO puts O in the textbox.
+        }
+
+        private void btnX_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "X";//btnX puts X in the textbox.
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "7";//btn7 puts 7 in the textbox.
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void btnA_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "A";//btnA puts A in the textbox.
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "0";//btn0 puts 0 in the textbox.
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "1";//btn1 puts 1 in the textbox.
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "2";//btn2 puts 2 in the textbox.
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "3";//btn3 puts 3 in the textbox.
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "4";//btn4 puts 4 in the textbox.
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "5";//btn5 puts 5 in the textbox.
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "6";//btn6 puts 6 in the textbox.
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "8";//btn8 puts 8 in the textbox.
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "9";//btn9 puts 9 in the textbox.
+        }
+
+        private void btnPeriod_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + ".";//btn. puts . in the textbox.
+        }
+
+        private void btnC_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "C";//btnC puts C in the textbox.
+        }
+
+        private void btnD_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "D";//btnD puts D in the textbox.
+        }
+
+        private void btnE_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "E";//btnE puts E in the textbox.
+        }
+
+        private void btnF_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "F";//btnF puts F in the textbox.
+        }
+
+        private void btnG_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "G";//btnG puts G in the textbox.
+        }
+
+        private void btnH_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "H";//btnH puts H in the textbox.
+        }
+
+        private void btnI_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "I";//btnI puts I in the textbox.
+        }
+
+        private void btnJ_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "J";//btnJ puts J in the textbox.
+        }
+
+        private void btnL_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "L";//btnL puts L in the textbox.
+        }
+
+        private void btnP_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "P";//btnP puts P in the textbox.
+        }
+
+        private void btnQ_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "Q";//btnQ puts Q in the textbox.
+        }
+
+        private void btnR_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "R";//btnR puts R in the textbox.
+        }
+
+        private void btnS_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "S";//btnS puts S in the textbox.
+        }
+
+        private void btnT_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "T";//btnT puts T in the textbox.
+        }
+
+        private void btnU_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "U";//btnU puts U in the textbox.
+        }
+
+        private void btnV_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "V";//btnV puts V in the textbox.
+        }
+
+        private void btnW_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "W";//btnW puts W in the textbox.
+        }
+
+        private void btnY_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "Y";//btnY puts Y in the textbox.
+        }
+
+        private void btnZ_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = txtBox1.Text + "Z";//btnZ puts Z in the textbox.
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtBox1.Text = "";//btnClear erases the textbox.
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            lbl1.Text = txtBox1.Text;//btnSend put the text from the textbox in the labelbox.
+            txtBox1.Text = "";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();//btnExit closes the program and exits.
+        }
+    }
+}
+//Rafael Gonzales
+//ITD-2343 Obj-Orient Prg Using C#
+//Spring 2026
+//Due:2/1/2026
